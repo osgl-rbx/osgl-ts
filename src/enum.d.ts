de250cff0d56808404aa8c67dccb3a139545d7e9
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-06 15:55:39 +0300

    Update enums

--- src/enum.d.ts
+++ src/enum.d.ts
@@ -1,3 +1,22 @@
-export enum WindowError {}
-export enum DrawableObjectError {}
-export enum TextureError {}
+declare namespace oEnum {
+    export enum WindowError {
+        InvalidCreationInstance = 0,
+        WindowDimensionsOutOfBounds = 1,
+        APINotEnabled = 8,
+    }
+
+    export enum DrawableObjectError {
+        NotEnoughMemory = 2,
+        ResizeOutOfBounds = 3,
+        BadBufferSize = 4,
+        OutOfBounds = 5,
+    }
+
+    export enum TextureError {
+        BadInput = 6,
+    }
+
+    export enum FontError {
+        BadInput = 7,
+    }
+}
