4cf74f8c8c9980a28045d07f2f1c0b976f3e1623
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-06 16:13:23 +0300

    Add `window.d.ts`

---
+++ src/DrawableObject/window.ts
@@ -0,0 +1,23 @@
+import { Result } from "../result";
+import { Drawable, OSGLWindow } from "../types";
+
+export declare const Window: {
+    new: (editableImage: EditableImage, renderers: Drawable[]) => OSGLWindow;
+
+    from: (
+        drawableObject: Drawable,
+        width: number,
+        height: number,
+    ) => Result<OSGLWindow, oEnum.WindowError>;
+
+    fromAssetId: (assetId: string) => Result<OSGLWindow, oEnum.WindowError>;
+
+    fromBuffer: (
+        buffer: buffer,
+        width: number,
+        height: number,
+    ) => Result<OSGLWindow, oEnum.WindowError>;
+
+    isDrawableInstance: (object: Instance) => boolean;
+    getRenderingProperty: (object: Drawable) => "ImageContent" | "TextureContent" | "";
+};
