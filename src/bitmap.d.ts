2f78961222b51a2aa588b2fa47b8efabc9242f4f
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-06 16:14:12 +0300

    Add `bitmap.d.ts`

---
+++ src/bitmap.d.ts
@@ -0,0 +1,7 @@
+import { Result } from "./result";
+import { RawTexture, OSGLBitmap, BaseUnloadedTexture } from "./types";
+
+export declare const Bitmap: {
+    fromChecked: (rawBitmap: RawTexture) => Result<OSGLBitmap, oEnum.TextureError>;
+    fromUnchecked: (rawBitmap: BaseUnloadedTexture) => OSGLBitmap;
+};
