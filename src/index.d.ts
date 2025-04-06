a6609aaf55099cb7e4445e584c6e6c68f6fa37bf
msix29 <abdulrehmanessam7567@gmail.com>
2025-04-06 16:37:46 +0300

    Update reexports

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,10 @@
-import { Window, Texture, Bitmap } from "./drawableObject";
+import { Window } from "./DrawableObject/window";
+import { Texture } from "./DrawableObject/texture";
+import { Bitmap } from "./bitmap";
 import { Font } from "./font";
 import { color } from "./color";
+import { Enum } from "./enum";
 
 declare module "OSGL" {
-    export { Window, Texture, Bitmap, Font, color };
+    export { Window, Texture, Bitmap, Enum, Font, color };
 }
